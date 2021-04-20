5742822f77e1ae9e41811697e9b77d3e5a2781e0
nirali25parekh <nirali25parekh@gmail.com>
2021-04-20 22:35:43 +0530

    save keys to file and read from file

--- final/generate_RSA_keys.py
+++ final/generate_RSA_keys.py
@@ -1,19 +1,27 @@
 import rsa
 
+# A's keys
 (A_pub, A_priv) = rsa.newkeys(512)
-print("A's public key and private key generated")
 
+A_pub_file = open("A_public_key.txt","w")
+A_pub_file.write(A_pub.save_pkcs1().decode('utf-8'))
+A_pub_file.close()
+
+A_priv_file = open("A_private_key.txt","w")
+A_priv_file.write(A_priv.save_pkcs1().decode('utf-8'))
+A_priv_file.close()
+
+print("A's public and private key saved in file")
+
+# B's keys
 (B_pub, B_priv) = rsa.newkeys(512)
-print("B's public and private key generated")
 
-def get_A_public_key:
-    return A_pub
+B_pub_file = open("B_public_key.txt","w")
+B_pub_file.write(B_pub.save_pkcs1().decode('utf-8'))
+B_pub_file.close()
 
-def get_B_public_key:
-    return B_pub
+B_priv_file = open("B_private_key.txt","w")
+B_priv_file.write(B_priv.save_pkcs1().decode('utf-8'))
+B_priv_file.close()
 
-def get_A_private_key:
-    return A_priv
-    
-def get_B_private_key:
-    return B_priv
+print("B's public and private key saved in file")
---
+++ final/rsa_key_getters.py
@@ -0,0 +1,24 @@
+def get_A_public_key():
+    A_pub_file = open('A_public_key.txt', mode='rb')
+    keydata = A_pub_file.read()
+    actualkey = rsa.PublicKey.load_pkcs1(keydata)
+    print(actualkey)
+    return actualkey
+
+def get_B_public_key():
+    B_pub_file = open('A_public_key.txt', mode='rb')
+    keydata = A_pub_file.read()
+    actualkey = rsa.PublicKey.load_pkcs1(keydata)
+    return actualkey
+
+def get_A_private_key():
+    A_priv_file = open('A_private_key.txt', mode='rb')
+    keydata = A_priv_file.read()
+    actualkey = rsa.PrivateKey.load_pkcs1(keydata)
+    return actualkey
+    
+def get_B_private_key():
+    B_priv_file = open('B_private_key.txt', mode='rb')
+    keydata = B_priv_file.read()
+    actualkey = rsa.PrivateKey.load_pkcs1(keydata)
+    return actualkey
