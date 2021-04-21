7878f7bcc30c9c2d4eff1c34919e833bbe28cc11
nirali25parekh <nirali25parekh@gmail.com>
2021-04-22 00:31:11 +0530

    rsa key getters

--- final/rsa_key_getters.py
+++ final/rsa_key_getters.py
@@ -1,3 +1,5 @@
+import rsa
+
 def get_A_public_key():
     A_pub_file = open('A_public_key.txt', mode='rb')
     keydata = A_pub_file.read()
@@ -6,8 +8,8 @@ def get_A_public_key():
     return actualkey
 
 def get_B_public_key():
-    B_pub_file = open('A_public_key.txt', mode='rb')
-    keydata = A_pub_file.read()
+    B_pub_file = open('B_public_key.txt', mode='rb')
+    keydata = B_pub_file.read()
     actualkey = rsa.PublicKey.load_pkcs1(keydata)
     return actualkey
 
